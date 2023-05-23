666a095b18ebbde1bd6bfe5201320acd6508e84b
Tapan Das <tapands.das@gmail.com>
2023-05-23 16:07:20 +0530

    Install firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import viteLogo from "/vite.svg";
-
 function App() {
   return <h1>Jay Jagannath</h1>;
 }
---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBpdoryq_z_RbucgEjxfXHleYORIIm5Twk",
+  authDomain: "seek-and-find-33dd4.firebaseapp.com",
+  projectId: "seek-and-find-33dd4",
+  storageBucket: "seek-and-find-33dd4.appspot.com",
+  messagingSenderId: "635412325658",
+  appId: "1:635412325658:web:300a1a7a39c7de9362b8a3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export { app };
