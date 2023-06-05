308e861dc3b8a119889a91ad37317ce728323816
Tapan Das <tapands.das@gmail.com>
2023-06-05 12:41:49 +0530

    Add custom font and change color scheme

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <main className="home min-h-screen bg-slate-200 p-2">
-      <h1 className="mb-5 text-center text-5xl font-bold">Seek & Find</h1>
-      <section className="flex items-start justify-between bg-purple-600">
+    <main className="home min-h-screen bg-gradient-to-r from-[#300063] to-[#9932CC] p-2">
+      <h1 className="mx-auto mb-7 w-80 font-chokokutai text-5xl font-bold text-white">
+        Seek & Find
+      </h1>
+      <section className="mx-auto flex items-start justify-between md:w-5/6">
         <button
           type="button"
-          className="rounded-md bg-amber-600 px-3 py-1.5 font-bold text-white"
+          className="rounded-md bg-[#00b300] px-3 py-1.5 font-bold text-white"
         >
           Leaderboard
         </button>
-        <div className="catalog grid min-w-fit grid-cols-1 gap-3 bg-green-400 p-3 md:grid-cols-3">
+        <div className="catalog grid min-w-fit grid-cols-1 gap-3 px-3 pb-3 pt-0 md:grid-cols-3">
           {catalog.map((item) => (
             <Link
               key={item.id}
@@ -20,14 +22,14 @@ const Home = () => {
               state={{ imgUrl: item.imgUrl }}
             >
               <div
-                className="card h-36 w-36 rounded-md bg-cover md:h-40 md:w-40 lg:h-44 lg:w-44"
+                className="card h-36 w-36 rounded-md bg-cover md:h-48 md:w-48 lg:h-56 lg:w-56"
                 style={{ backgroundImage: `url(${item.iconUrl})` }}
               ></div>
             </Link>
           ))}
         </div>
-        <div className="instruction w-72 rounded-md bg-fuchsia-400 p-2 text-center text-white">
-          <h3 className="text-2xl font-bold text-black">Instructions</h3>
+        <div className="instruction w-72 rounded-md bg-[#cc0066] p-2 text-center text-white">
+          <h2 className="text-3xl font-bold text-white">Instructions</h2>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
           quis cum possimus magnam ex minus non error dolor quibusdam cupiditate
           quod commodi provident sunt obcaecati animi, voluptates officia
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      chokokutai: ["Chokokutai"],
+      wallpoet: ["Wallpoet"],
+    },
     extend: {},
   },
   plugins: [],
