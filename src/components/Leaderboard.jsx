06cbd3a2250d37a260e33c38e39e173677564aae
Tapan Das <tapands.das@gmail.com>
2023-07-21 13:08:46 +0530

    Read Leaderboard data from FB

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,6 +1,34 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { db } from "../firebase-config";
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
 
 const Leaderboard = () => {
+  useEffect(() => {
+    const fetchUserScore = async () => {
+      try {
+        const collectionRef = collection(db, "leaderboard");
+        const q = query(collectionRef, orderBy("time"));
+        const querySnapshot = await getDocs(q);
+        const scoreList = [];
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          const { userId, userName, time, canvas } = data;
+          scoreList.push({
+            userId,
+            userName,
+            canvas,
+            time,
+          });
+        });
+        console.table(scoreList);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchUserScore();
+  }, []);
+
   return (
     <>
       <Link to="/">
