111b121202ea703769301c6082fa786e022abbf8
Tapan Das <tapands.das@gmail.com>
2023-07-29 11:26:50 +0530

    Add an useEffect in Found
    To check the number of fetch calls

--- src/components/Found.jsx
+++ src/components/Found.jsx
@@ -1,23 +1,25 @@
 import PropTypes from "prop-types";
 import checkCoordinates from "../helper/checkCoordinates";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Found = ({ id, picName, isFound, getImageDimension }) => {
   const [markingStyle, setMarkingStyle] = useState({ left: "0px", top: "0px" });
 
-  checkCoordinates(picName, id, { x: 0, y: 0 })
-    .then(({ targetDimension }) => {
-      const { imageWidth, imageHeight } = getImageDimension();
-      const { xMin, xMax, yMin, yMax } = targetDimension;
-      const xMid = (xMin + xMax) / 2;
-      const yMid = (yMin + yMax) / 2;
-      const updatedPosition = {
-        left: `${imageWidth * xMid - 40}` + "px", // 15px for centering
-        top: `${imageHeight * yMid + 20}` + "px", // 60px for nav bar
-      };
-      setMarkingStyle(updatedPosition);
-    })
-    .catch((error) => console.error(error));
+  useEffect(() => {
+    checkCoordinates(picName, id, { x: 0, y: 0 })
+      .then(({ targetDimension }) => {
+        const { imageWidth, imageHeight } = getImageDimension();
+        const { xMin, xMax, yMin, yMax } = targetDimension;
+        const xMid = (xMin + xMax) / 2;
+        const yMid = (yMin + yMax) / 2;
+        const updatedPosition = {
+          left: `${imageWidth * xMid - 40}` + "px", // 15px for centering
+          top: `${imageHeight * yMid + 20}` + "px", // 60px for nav bar
+        };
+        setMarkingStyle(updatedPosition);
+      })
+      .catch((error) => console.error(error));
+  }, [id, picName, getImageDimension]);
 
   return isFound ? (
     <img
